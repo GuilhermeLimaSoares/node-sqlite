4b6a91686e8eb63f876dea0aa4c7242527e8c3f4
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2024-05-12 16:03:26 -0300

    refactor: Add Services layer

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const routes = require('../api/routes');
 
 const app = express();
 
 app.use(express.json());
+routes(app);
 
-app.get('/teste', (req, res) => {
-  res
-    .status(200)
-    .send({ mensagem: 'boas-vindas à API' });
-});
+// app.get('/teste', (req, res) => {
+//   res
+//     .status(200)
+//     .send({ mensagem: 'boas-vindas à API' });
+// });
 
 module.exports = app;
